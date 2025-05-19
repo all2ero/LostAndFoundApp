d77e17cfb3d07f6099c8b6326990afa284983c30
handayoung <amydyhan@gmail.com>
2025-05-20 02:49:25 +0900

    분실물 정보를 저장하는 LostItem 클래스 구현 (id, 이름, 장소, 날짜, 연락처 포함)

--- LostFoundApp/src/Main.java
+++ LostFoundApp/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -36,5 +37,26 @@ public class Main {
         }
 
         scanner.close();
+    }
+}
+
+class LostItem {
+    private static int count = 1;
+    private int id;
+    private String name;
+    private String place;
+    private String date;
+    private String contact;
+
+    public LostItem(String name, String place, String date, String contact) {
+        this.id = count++;
+        this.name = name;
+        this.place = place;
+        this.date = date;
+        this.contact = contact;
+    }
+
+    public String toString() {
+        return "[" + id + "] " + name + " / 위치: " + place + " / 날짜: " + date + " / 연락처: " + contact;
     }
 }
